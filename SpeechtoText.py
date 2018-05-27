ef06dfff22910b7884e2e2f9881394548a45ba8f
gupta17anukriti <gupta17anukriti@optum.com>
2018-05-27 23:16:03 +0530

    Update and rename Basic to SpeechtoText.py

--- SpeechtoText.py
+++ SpeechtoText.py
@@ -4,12 +4,14 @@ r = sr.Recognizer()
 r.pause_threshold = 1.0
 r.phrase_threshold = 1.0
 r.non_speaking_duration = 1.0
+r.energy_threshold =1000
 
 ##For live voice use 
 #with sr.Microphone() as source:
 with sr.AudioFile("G:\Dev\SpeechToText\d.wav") as source:
 print("Say something!")
-audio = r.listen(source)
+#listen for microphone, record for .wav 
+audio = r.record(source)
 
 try:
 # for testing purposes, we're just using the default API key
@@ -20,5 +22,3 @@ except sr.UnknownValueError:
 print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
 raise print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-
